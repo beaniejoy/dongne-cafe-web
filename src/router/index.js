3966d9b158c5dc10c15134fc3311c373e7e16db9
Hanbin Lee <lhb1025@gmail.com>
2023-01-16 00:04:24 +0900

    [#2] feat: migrate from vue2 to vue3
    - 전체적인 프로젝트 설정
    - vuex, vue-router, vue-cookies, vuetify 새로 설정
    - tailwind.css 적용 안되는 문제 있음(진행중)

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,7 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-const HomeView = () => import(/* webpackChunkName: "home" */ '@/views/HomeView.vue');
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
 const LoginView = () => import(/* webpackChunkName: "login" */ '@/views/auth/LoginView.vue');
 const SignupView = () => import(/* webpackChunkName: "signup" */ '@/views/auth/SignupView.vue');
-
-Vue.use(VueRouter);
 
 const routes = [
   {
@@ -24,8 +21,8 @@ const routes = [
   },
 ];
 
-const router = new VueRouter({
-  mode: 'history',
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
   routes
 });
 
--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,36 +1,31 @@
 <template>
-  <v-main class="mx-auto d-flex align-center">
-    <v-content>
-      <v-container>
+  <v-main class="grid content-center">
+    <v-card class="mx-auto" max-width="380" elevation="6">
+      <v-img
+        class="align-end text-white"
+        width="380px"
+        max-height="300px"
+        src="@/assets/img/main_login.jpg"
+        eager
+      >
+        <v-card-title class="ml-3 mb-1">
+          우리 동네 카페
+        </v-card-title>
+      </v-img>
 
-        <v-card :max-width="size" elevation="6">
-          <v-img
-            class="white--text align-end"
-            max-height="300px"
-            src="@/assets/img/main_login.jpg"
-            eager
-          >
-            <v-card-title class="ml-3 mb-1">
-              <span class="card-title">우리 동네 카페</span>
-            </v-card-title>
-          </v-img>
+      <v-card-text class="pb-0">
+        <!-- LoginForm Component -->
+        <login-form />
+      </v-card-text>
 
-          <v-card-text class="pb-0">
-            <!-- LoginForm Component -->
-            <login-form />
-          </v-card-text>
-
-          <v-card-actions>
-            <v-spacer />
-          
-            <router-link class="pr-2 pb-2" to="/signup">
-              <span class="text-amber-700">회원가입</span>
-            </router-link>
-          </v-card-actions>
-        </v-card>
-
-      </v-container>
-    </v-content>
+      <v-card-actions>
+        <v-spacer />
+      
+        <router-link class="pr-2 pb-2" to="/signup">
+          <span class="text-amber-700">회원가입</span>
+        </router-link>
+      </v-card-actions>
+    </v-card>
   </v-main>
 </template>
 
@@ -42,23 +37,9 @@ export default {
   components: {
     LoginForm
   },
-  computed: {
-    size() {
-      switch (this.$vuetify.breakpoint.name) {
-      case 'xs':
-      case 'sm':
-      case 'md':
-      case 'lg':
-      case 'xl':
-        return 380;
-      default:
-        return 0;
-      }
-    }
-  },
   created() {
     
-  }
+  },
 };
 </script>
 
