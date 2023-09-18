9986c92e5aa91711fa932d74ac93f873294d3cd8
beaniejoy <lhb1025@gmail.com>
2023-09-19 01:08:28 +0900

    [#12] feat: 카페 상세페이지 내 카테고리, 메뉴 추가

--- src/views/cafe/CafeDetailView.vue
+++ src/views/cafe/CafeDetailView.vue
@@ -8,9 +8,10 @@
       height="100%"
     >
       <v-carousel
+        cycle
         height="250"
+        show-arrows="hover"
         hide-delimiter-background
-        hide-delimiters
       >
         <v-carousel-item
           v-for="(image, i) in cafeDetail.cafeImages"
@@ -22,12 +23,13 @@
       </v-carousel>
 
       <v-card-item>
-        <v-card-title>
+        <v-card-title class="my-2">
+          <v-icon icon="mdi-coffee" class="mr-2" />
           {{ $filters.cafeNameWhiteSpace(cafeDetail.name) }}
         </v-card-title>
       </v-card-item>
 
-      <v-card-text>
+      <v-card-text class="my-2">
         <v-row
           align="center"
           class="mx-0"
@@ -41,43 +43,53 @@
             size="small"
           />
           <div class="text-grey ms-4">
-            {{ cafeDetail.totalRate }} (413)
+            {{ cafeDetail.totalRate }}
           </div>
         </v-row>
       </v-card-text>
 
-      <v-card 
-        v-for="(menu, i) in cafeDetail.menus" 
-        :key="`menu_${i}`"
-        elevation="0"
+      <v-divider class="mx-4 mb-1" />
+
+      <v-sheet
+        v-for="category in cafeDetail.cafeMenuCategories" 
+        :key="`category_${category.menuCategoryId}`"
+        border
+        class="py-4"
       >
-        <div class="d-flex flex-no-wrap justify-space-between">
-          <div>
-            <v-card-title>
-              {{ menu.name }}
-            </v-card-title>
+        <div class="mb-4 px-2">
+          <v-card-title>
+            {{ category.name }}
+          </v-card-title>
 
-            <v-card-subtitle>{{ menu.price }}</v-card-subtitle>
-          </div>
-
-          <v-avatar
-            class="ma-3"
-            size="125"
-            rounded="0"
-          >
-            <v-img src="https://cdn.vuetifyjs.com/images/cards/foster.jpg" />
-          </v-avatar>
+          <v-card-subtitle>{{ category.description }}</v-card-subtitle>
         </div>
-      </v-card>
+        <div>
+          <v-card 
+            v-for="menu in category.cafeMenus"
+            :key="`menu_${menu.cafeMenuId}`"
+            elevation="0"
+          >
+            <div class="flex flex-nowrap justify-around">
+              <v-avatar
+                class="ma-3"
+                size="125"
+                rounded="0"
+              >
+                <v-img src="https://cdn.vuetifyjs.com/images/cards/foster.jpg" />
+              </v-avatar>
+          
+              <div>
+                <v-card-title>
+                  {{ menu.name }}
+                </v-card-title>
 
-      <v-card-actions>
-        <v-btn
-          color="deep-purple-lighten-2"
-          variant="text"
-        >
-          Reserve
-        </v-btn>
-      </v-card-actions>
+                <v-card-subtitle>{{ $filters.currencyKOR(menu.price) }}</v-card-subtitle>
+                <v-card-subtitle>{{ menu.description }}</v-card-subtitle>
+              </div>
+            </div>
+          </v-card>
+        </div>
+      </v-sheet>
     </v-card>
   </v-main>
 </template>
